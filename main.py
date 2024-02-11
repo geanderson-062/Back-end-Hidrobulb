2dc47e8a5a8cf5feb5362e376e9996dcade6993a
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-11 03:24:23 -0300

    redirect to docs

--- main.py
+++ main.py
@@ -1,9 +1,16 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, HTTPException
+from fastapi.responses import RedirectResponse
 from endpoints.router import router
 
 app = FastAPI()
 
 app.include_router(router)
+
+
+@app.get("/", include_in_schema=False)
+async def redirect_to_docs():
+    return RedirectResponse(url="/docs")
+
 
 if __name__ == "__main__":
     import uvicorn
