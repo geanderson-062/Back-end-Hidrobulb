889205b71e17b1bba76eb65f127cce7a957addd6
geanderson-062 <83518241+geanderson-062@users.noreply.github.com>
2024-02-13 18:53:33 -0300

    cors

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import RedirectResponse
+from fastapi.middleware.cors import CORSMiddleware
 from routers.router import router
 
 app = FastAPI(
     title="API de simulação do Bulbo em camada superficial",
     description="Esta api fornece informações sobre os dados necessários para configurar uma simulação da camada superficial do bulbo úmido no software HYDRUS 3D. Ele descreve as propriedades do solo, as condições iniciais, as condições de contorno, os parâmetros climáticos, a geometria do domínio, o intervalo de tempo e as condições de saturação e drenagem.",
+)
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["GET"],
+    allow_headers=["*"],
 )
 
 app.include_router(router)
